9c7adc44a94cd1852e3bc15083803cf1ef26625e
Lauri Tervonen <lauri.tervonen@aalto.fi>
2019-02-02 13:31:09 +0000

    Implement cumulative reporting mode

--- main.py
+++ main.py
@@ -30,10 +30,7 @@ def evaluate(input_string):
     literal_str = DiceHandler()(input_string)
     return numexpr.evaluate(literal_str)
 
-def simulate(input_string):
-
-
-
+def simulate(input_string, report_cumulative=False):
     results = {}
     count = 0
     try:
@@ -46,22 +43,35 @@ def simulate(input_string):
                 results[result] = 1
 
             if (count+1)%1000 == 0:
-                print_height = print_graph(results)
+                print_height = print_graph(results, report_cumulative)
                 print(f"\033[F"*(len(print_height)+2))
     except KeyboardInterrupt:
-        print_graph(results)
+        print_graph(results, report_cumulative)
         return
 
-def print_graph(results):
-    maxvalue = max(results.values())
-    maxvalue_len = max(len(str(i)) for i in results.values())
-    total_bar_length = shutil.get_terminal_size((80,20))[0]-maxvalue_len-12
-    count = sum(results.values())
+def results_to_cumulative(results):
+    sorted_keys = sorted(results.keys())
+    cumulative = {sorted_keys[i]:sum(   results[j]
+                                        for j in sorted_keys[:i+1])
+                                        for i in range(len(sorted_keys))}
+    return cumulative
 
-    print("Expected value: {:6.3f}".format(sum((i/count)*results[i] for i in results)))
+def print_graph(results, cumulative=False):
+    print("Expected value: {:6.3f}".format(sum((i/sum(results.values()))*results[i] for i in results)))
+
+    if cumulative:
+        results = results_to_cumulative(results)
+        count = max(results.values())
+    else:
+        count = sum(results.values())
+
+    maxvalue = max(results.values())
+    maxvalue_len = max(len(str(i)) for i in results.keys())
+    total_bar_length = shutil.get_terminal_size((80,20))[0]-maxvalue_len-12
+
     for key in sorted(results.keys()):
         bar_length = round(results[key]/maxvalue*total_bar_length)
-        print(("{:<"+ str(maxvalue_len)+"}{:6.3f}%  {}").format(
+        print(("{:<"+ str(maxvalue_len)+"}{:7.3f}%  {}").format(
                         str(key)+":",
                         results[key]/count*100,
                         u"\u2588"*bar_length + " "*(total_bar_length-bar_length+1)
@@ -69,7 +79,10 @@ def print_graph(results):
     return results.keys()
 
 def main(arg_string):
-    simulate(arg_string)
+    if arg_string[0] == "C":
+        simulate(arg_string[1:], report_cumulative=True)
+    else:
+        simulate(arg_string, report_cumulative=False)
 
 if __name__ == "__main__":
     main(" ".join(sys.argv[1:]))
